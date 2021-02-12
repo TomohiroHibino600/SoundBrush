51e315c75ea53f03f1d5f9f7355c54a1e6581df5
TomohiroHibino600 <tomohiro.hibino@edoga.jp>
2021-02-12 14:17:44 +0900

    add project
    - add audio visualization
    - add MLAPI Settings

---
+++ Assets/DissapairHint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissapairHint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke(nameof(Dissapair), 20f);
+    }
+
+    void Dissapair()
+    {
+        transform.gameObject.SetActive(false);
+    }
+}
